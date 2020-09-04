f9f6a63627656d43f55c01a5b681818a1c69662b
leonard <leonardllzznn@gmail.com>
2020-09-04 10:43:34 +0800

    a test commit

---
+++ Lab1/boyer_moore.py
@@ -0,0 +1,17 @@
+# TODO: boyer moore algo implementation and testing 
+
+def boyer_moore():
+    """ implementation goes here
+    """
+    print("BOYER MOORE")
+    return
+
+
+def run():
+    """ testing goes here
+    """
+    boyer_moore()
+
+
+if __name__ == "__main__":
+    run()
